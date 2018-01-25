8847a9717a14d847f446ce9dba11abcd9229a968
Alexander Stone <enderu@gmail.com>
2018-01-25 10:57:22 -0600

    Lots of cleanup and adding two levels of logging (verbose and noisy).

--- screenshot_retimer.py
+++ screenshot_retimer.py
@@ -24,12 +24,12 @@ parser = argparse.ArgumentParser(description='Correct the file creation and modi
 parser.add_argument('directory', help='Path to the directory containing the images.')
 parser.add_argument('-e', '--execute', action='store_true', help='Modifiy the files.')
 parser.add_argument('-r', '--rename', action='store_true', help='Rename the files to First Last YYYYMMDD_HHMMSS.')
-parser.add_argument('-v', '--verbose', action='store_true', help='Display additional logging.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', action='store_true', help='Display additional logging.')
+group.add_argument('-n', '--noisy', action='store_true', help='Display a huge amount of logging.')
 parser.add_argument('-c', '--creation_time', action='store_true', help='Use the macOS SetFile command to also set file creation time. It\'s slow!!')
 
 args = parser.parse_args()
-
-
 
 def correct_file_time(filename, datetime_string):
     time_struct = time.strptime(datetime_string, '%m/%d/%Y %H:%M:%S')
@@ -45,19 +45,17 @@ def correct_file_time_setfile(filename, datetime_string):
     os.system('SetFile -dm "{}" {}'.format(datetime_string, full_file_path.replace(' ', '\\ ')))
 
 
-
 if args.creation_time:
     if not which('SetFile'):
-        print('SetFile flag was used, but SetFile command cannot be found.')
+        print('Creation time flag (-c) was used, but SetFile command cannot be found.')
         print('Install Xcode command line tools from https://developer.apple.com')
         exit(1)
-    elif args.verbose:
+    elif args.verbose or args.noisy:
         print('SetFile command found:',which('SetFile'))
 
 path = os.path.expanduser(args.directory)
-if args.verbose:
-    print('Checking directory {}'.format(path))
-
+if args.verbose or args.noisy:
+    print('Checking directory {}.'.format(path))
 dir_list = os.scandir(path)
 
 files = []
@@ -65,45 +63,31 @@ for item in dir_list:
     if item.is_file() and not item.name.startswith('.'):
         files.append(re.split(' |_|\.',item.name) + [item.name])
 
-invalid_files = False
+
+# Validate the files to build images and invalid files lists.
+images = []
+invalid_files = []
+
+if args.verbose or args.noisy:
+    print('Checking files for invalid file names and extensions.')
 for f in files:
+    filename = f[-1]
     if len(f) != 10:
-        if not invalid_files:
-            print('Filename pattern changed. Code needs correcting. Exiting without modifying files.')
-            print('Filenames should be in format: \'First Last MM_DD_YYYY HH_MM_SS.jpg\'.')
-            invalid_files = True
-        print('Invalid filename: {}'.format(f[-1]))
+        invalid_files.append(filename)        
+        if args.verbose or args.noisy:
+            print('Invalid filename: \'{}\''.format(filename))
+    elif filename[-3:] not in ['jpg', 'png']:
+        invalid_files.append(filename)
+        if args.verbose or args.noisy:
+            print('Invalid extension: \'{}\''.format(filename))
+    else:
+        images.append(f)
+if (args.verbose or args.noisy):
+    if not len(invalid_files):
+        print('None found.')
+    else:
+        print()
 
-if invalid_files:
-    exit(1)
-
-# invalid_files = []
-# for f in files:
-#     filename = f[-1]
-#     if len(f) != 10:
-#         print('Invalid filename: {}'.format(filename))
-#         invalid_files.append(filename)
-#     elif filename[-3:] not in ['jpg', 'png']:
-#         print('Invalid extension: {}'.format(filename))
-#         invalid_files.append(filename)
-#     else:
-#         images.append(f)
-
-
-# filter out movies or other files
-images = [f for f in files if f[8] in ['jpg', 'png']]
-
-# if args.verbose:
-#     # print(set([image[9] for image in images]))
-#     excluded_files = list(set(dir_list) - set([image[9] for image in images]))
-#     if len(excluded_files) > 0:
-#         print('The following files were excluded:')
-#         for ex in excluded_files:
-#             print('--- {}'.format(ex))
-
-
-if args.verbose:
-    print('Adjusting file creation/modification times.')
 
 for i in images:
     [first, last, month, day, year, hours, minutes, seconds, extension, original_filename] = i
@@ -111,8 +95,8 @@ for i in images:
     time_string = '{}/{}/{} {}:{}:{}'.format(month, day, year, hours, minutes, seconds)
     time_string_sane = '{}/{}/{} {}:{}:{}'.format(year, month, day, hours, minutes, seconds)
     new_filename = '{} {} {}{}{}_{}{}{}.{}'.format(first, last, year, month, day, hours, minutes, seconds, extension)
-    
-    if args.verbose:
+
+    if args.noisy:
         print('\'{}\''.format(original_filename))
         print('\tRetiming to {}{}'.format(time_string_sane, '\n' if not args.rename else ''))
     
@@ -123,14 +107,20 @@ for i in images:
             correct_file_time(original_filename, time_string)
 
     if args.rename:
-        if args.verbose:
+        if args.noisy:
             print('\tRenaming to \'{}\'\n'.format(new_filename))
         if args.execute:
             os.rename(path + original_filename, path + new_filename)
 
 end_time = timeit.default_timer()
-print('Completed in {0} seconds.\n'.format(round(end_time-start_time,2)))
+print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
 print('{} files {} {}{}.'.format(len(images), 'were' if args.execute else 'will be', 'renamed and retimed' if args.rename else 'retimed', ' using SetFile' if args.creation_time else ''))
 
+if len(invalid_files) > 0:
+    print('\nThe following files {} processed. {}.'.format('weren\'t' if args.execute else 'won\'t be', 'See above' if (args.verbose or args.noisy) else 'Run with the -v flag for more details'))
+    for invalid_file in invalid_files:
+        print('\t\'{}\''.format(invalid_file))
+    print()
+
 if not args.execute:
-    print('DRY RUN COMPLETE. No files were modified. Run command with -e flag to execute.')
+    print('DRY RUN COMPLETE. No files were modified. Run with -e flag to execute.')

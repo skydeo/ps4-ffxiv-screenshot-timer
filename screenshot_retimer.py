6fa7e9dd236651e08d2fe4dba026cc87fd2f3c5a
Alexander Stone <enderu@gmail.com>
2019-07-15 15:15:17 -0500

    huge refactor. created a Screenshot class Screenshot objects contain file path, all versions of file names and datetime strings, and methods to retime and rename object

--- screenshot_retimer.py
+++ screenshot_retimer.py
@@ -6,29 +6,47 @@ import shutil
 from pathlib import Path
 from gooey import Gooey, GooeyParser
 
+class Screenshot:
+  ''' An object to hold a screenshot (or video) that should be renamed or retimed '''
+
+  def __init__(self, file_path):
+    self.original_filename = file_path.name
+    self.file_path = file_path
+
+    parts = re.split(r' |_|\.', self.file_path.name)
+    if len(parts) == 9:
+      [first, last, month, day, year, hours, minutes, seconds, _] = parts
+      name = f'{first} {last}'
+    elif len(parts) == 8:
+      [first, month, day, year, hours, minutes, seconds, _] = parts
+      name = first
+
+    self.timestamp = f'{year}{month}{day}_{hours}{minutes}{seconds}'
+    self.new_filename = f'{name} {self.timestamp}{self.file_path.suffix}'
+    self.setfile_timestamp = f'{month}/{day}/{year} {hours}:{minutes}:{seconds}'
+
+    if self.file_path.suffix.lower() in ['.jpg', '.png']:
+      self.type = 'image'
+    elif self.file_path.suffix.lower() in ['.mp4']:
+      self.type = 'video'
+    else:
+      self.type = 'unknown'
+  
+  def correct_file_time(self):
+    time_struct = time.strptime(self.timestamp, '%Y%m%d_%H%M%S')
+    seconds_since = int(time.mktime(time_struct))
+    os.utime(self.file_path, (seconds_since, seconds_since))
+  
+  def correct_file_time_setfile(self):
+    os.system('SetFile -dm "{}" {}'.format(self.setfile_timestamp, self.file_path.replace(' ', '\\ ')))
+  
+  def rename_file(self):
+    os.rename(self.file_path.parents[0] / self.original_filename, self.file_path.parents[0] / self.new_filename)
 
 
 def retime_ps4_screenshots(input_dir, execute=False, rename=False, setfile=False, verbose=True):
   start_time = timeit.default_timer()
   input_dir = Path(input_dir)
-
-
-  def create_dir_list(input_dir):
-    p = Path(input_dir).iterdir()
-    dir_list = [entry for entry in p 
-                if entry.is_file()
-                and not entry.name.startswith('.')]
-    return dir_list
-
-
-  def correct_file_time(filename, datetime_string):
-    time_struct = time.strptime(datetime_string, '%m/%d/%Y %H:%M:%S')
-    seconds_since = int(time.mktime(time_struct))
-    os.utime(filename, (seconds_since, seconds_since))
-
-
-  def correct_file_time_setfile(filename, datetime_string):
-    os.system('SetFile -dm "{}" {}'.format(datetime_string, filename.replace(' ', '\\ ')))
 
 
   if setfile:
@@ -39,91 +57,62 @@ def retime_ps4_screenshots(input_dir, execute=False, rename=False, setfile=False
     elif verbose:
       print(f'SetFile command found: {shutil.which("SetFile")}')
 
-  # path = os.path.expanduser(input_dir)
+
+  def create_dir_list(input_dir):
+    p = Path(input_dir).iterdir()
+    dir_list = [entry for entry in p 
+                if entry.is_file()
+                and not entry.name.startswith('.')]
+    return dir_list
+
+
   if verbose:
     print(f'Checking directory {input_dir}')
   dir_list = create_dir_list(input_dir)
 
   files = []
-  videos = []
   for item in dir_list:
-    if item.suffix.lower() in ['.jpg', '.png']:
-      files.append(re.split(r' |_|\.',item.name) + [item])
-    elif item.suffix.lower() == '.mp4':
-      videos.append(re.split(r' |_|\.',item.name) + [item])
-  
-
-  '''
-  ['Xander', 'Barabroda', '06', '18', '2017', '13', '37', '51', 'jpg']
-  0 = First name
-  1 = Last name
-  2 = Month
-  3 = Day
-  4 = Year
-  5 = Hour
-  6 = Minute
-  7 = Second
-  8 = extension
-  '''
+    if item.is_file():
+      files.append(Screenshot(item))
 
   # Validate the files to build images and invalid files lists.
-  images = []
-  invalid_files = []
-
   if verbose:
     print('Checking files for invalid file names and extensions.')
-  for f in files:
-    dir_entry = f[-1]
-    if len(f) != 10:
-      invalid_files.append(dir_entry)
-      if verbose:
-        print('Invalid filename: \'{}\''.format(dir_entry))
-    elif dir_entry.suffix not in ['.jpg', '.png', '.mp4']:
-      invalid_files.append(dir_entry)
-      if verbose:
-        print('Invalid extension: \'{}\''.format(dir_entry.name))
-    else:
-      images.append(f)
-  if verbose:
-    if not len(invalid_files):
-      print('None found.')
-    else:
-      print()
+
+  images = [f for f in files if f.type == 'image']
+  videos = [f for f in files if f.type == 'video']
+  unknowns = [f for f in files if f.type == 'unknown']
 
   if verbose:
+    print(f'Found {len(images)} screenshots, {len(videos)} videos, and {len(unknowns)} unknown file types.')
     print('Retiming images.')
 
-
-  for i in images:
-    [first, last, month, day, year, hours, minutes, seconds, extension, original_filename] = i
-
-    time_string = f'{month}/{day}/{year} {hours}:{minutes}:{seconds}'
-    time_string_sane = f'{year}/{month}/{day} {hours}:{minutes}:{seconds}'
-    new_filename = input_dir / f'{first} {last} {year}{month}{day}_{hours}{minutes}{seconds}.{extension}'
-
-    if verbose == 2:
-      print('\'{}\''.format(original_filename))
-      print('\tRetiming to {}{}'.format(time_string_sane, '\n' if not rename else ''))
+  for image in images:
+    # if verbose == 2:
+    if verbose:
+      print(f"'{image.original_filename}\'")
+      print('\tRetiming to {}{}'.format(image.setfile_timestamp, '\n' if not rename else ''))
 
     if execute:
       if setfile:
-        correct_file_time_setfile(original_filename, time_string)
-      correct_file_time(original_filename, time_string)
+        image.correct_file_time_setfile()
+      image.correct_file_time()
 
     if rename:
-      if verbose == 2:
-        print('\tRenaming to \'{}\'\n'.format(new_filename))
+      # if verbose == 2:
+      if verbose:
+        print(f"\tRenaming to '{image.new_filename}'\n")
       if execute:
-        os.rename(original_filename, new_filename)
+        image.rename_file()
 
   end_time = timeit.default_timer()
   print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
   print('{} files {} {}{}.'.format(len(images), 'were' if execute else 'will be', 'renamed and retimed' if rename else 'retimed', ' using SetFile' if setfile else ''))
 
-  if len(invalid_files) > 0:
-    print('\nThe following {} {} {} processed. {}.'.format(len(invalid_files),'files' if len(invalid_files) != 1 else 'file','weren\'t' if execute else 'won\'t be', 'See above' if verbose else 'Run with the -v flag for more details'))
-    for invalid_file in invalid_files:
-      print('\t\'{}\''.format(invalid_file))
+  if len(unknowns) > 0:
+    print('\nThe following {} {} {} processed. {}.'.format(len(unknowns),'files' if len(unknowns) != 1 else 'file','weren\'t' if execute else 'won\'t be', 'See above' if verbose else 'Run with the -v flag for more details'))
+    for unknown in unknowns:
+      print(f"\t'{unknown.name}'")
     print()
 
   if not execute:
